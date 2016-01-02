30e2d97de5cea00081d2100bf0509d640291f83b
bskydive <stepanovv.ru@yandex.ru>
2016-01-03 01:05:57 +0300

    feature-3.9.4.0 improve navbar design, delete empty columns

--- portfolio/public/js/portfolio.js
+++ portfolio/public/js/portfolio.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-    $(".row-header").addClass("text-center");
-    $(".column-left").addClass("col-md-1");
-    $(".column-right").addClass("col-md-1");
+    //$(".row-header").addClass("text-center");
+    //$(".column-left").addClass("col-md-1");
+    //$(".column-right").addClass("col-md-1");
     $(".column-double-center").addClass("col-md-8");
     $(".column-left-center").addClass("col-md-7");
     $(".column-right-center").addClass("col-md-1");
@@ -12,10 +12,10 @@ $(document).ready(function () {
     //$(".column-double-center").addClass("bg-overlay");
 
 
-        $(document.body).css('padding-top', $('#topnavbar').height() + 100);
-        $(window).resize(function(){
-            $(document.body).css('padding-top', $('#topnavbar').height() + 100);
-        });
+        //$(document.body).css('padding-top', $('#topnavbar').height() + 100);
+        //$(window).resize(function(){
+        //    $(document.body).css('padding-top', $('#topnavbar').height() + 100);
+        //});
 
 });
 
