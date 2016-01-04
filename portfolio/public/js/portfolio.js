a433de93a4a0a48a2edb9da102fed30616a078a7
bskydive <stepanovv.ru@yandex.ru>
2016-01-05 01:52:49 +0300

    feature-3.9.4.0 improve main layout, remove bg

--- portfolio/public/js/portfolio.js
+++ portfolio/public/js/portfolio.js
@@ -2,9 +2,10 @@ $(document).ready(function () {
     //$(".row-header").addClass("text-center");
     //$(".column-left").addClass("col-md-1");
     //$(".column-right").addClass("col-md-1");
-    $(".column-double-center").addClass("col-md-8");
-    $(".column-left-center").addClass("col-md-7");
-    $(".column-right-center").addClass("col-md-1");
+    $(".column-double-center").addClass("col-md-12");
+    $(".c-columnNavTop").addClass("col-md-12");
+    //$(".column-left-center").addClass("col-md-7");
+    //$(".column-right-center").addClass("col-md-3");
 
 
     //$(".container-main").addClass("container-fluid col-md-8");
@@ -12,10 +13,10 @@ $(document).ready(function () {
     //$(".column-double-center").addClass("bg-overlay");
 
 
-        //$(document.body).css('padding-top', $('#topnavbar').height() + 100);
-        //$(window).resize(function(){
-        //    $(document.body).css('padding-top', $('#topnavbar').height() + 100);
-        //});
+        $("#id_containerMain").css('padding-top', $('.c-columnNavTop').height() + 100);
+        $(window).resize(function(){
+            $("#id_containerMain").css('padding-top', $('.c-columnNavTop').height() + 100);
+        });
 
 });
 
