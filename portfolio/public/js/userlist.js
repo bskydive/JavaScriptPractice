f5c6114da246ddf7a768f12a3bdbffaa8e4d7f02
bskydive <stepanovv.ru@yandex.ru>
2016-01-17 23:25:24 +0300

    feature-4.0.0.0 improve userlist app, userlist view implemented

--- portfolio/public/js/userlist.js
+++ portfolio/public/js/userlist.js
@@ -1,7 +1,7 @@
-//window.$(document).ready(function () {
-(function ($) {
+window.$(document).ready(function () {
+
     "use strict";
-    //var $ = window.$, Backbone = window.Backbone, console = window.console, _ = window._;
+    var $ = window.$, Backbone = window.Backbone, console = window.console, _ = window._;
 
     $(".c-column-center").addClass("col-md-12");
     $(".c-column-userList").addClass("col-md-3");
@@ -21,14 +21,9 @@
     //    offsetResize();
     //});
 
-    // Generate four random hex digits.
-    function S4() {
-        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-    }
+    function genUid() {
 
-    // Generate a pseudo-GUID by concatenating random hexadecimal.
-    function guid() {
-        return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+            return window.Date.now().toString() +"-" + Math.random().toString(36).substring(2, 15) + "-" + Math.random().toString(36).substring(2, 15);
     }
 
     //============================================MODEL======================================================
@@ -64,37 +59,37 @@
             eMail: "blankEMail@stepanovv.ru",
             phoneNumber: "+71112233344",
             //order: userListView1.nextOrder(),
-            guidNumber: guid()
+            uidNumber: genUid()
+            },
+
+        validate: function (attrs) {
+
+            if (!checkStringLength(attrs.firstName, 3, 25)) {
+                return "First name" + this.guidNumber.toString() + " should be 3 to 25 letters long";
             }
 
-        //validate: function (attrs) {
-        //
-        //    if (!checkStringLength(attrs.firstName, 3, 25)) {
-        //        return "First name" + this.guidNumber.toString() + " should be 3 to 25 letters long";
-        //    }
-        //
-        //    if (!checkStringLength(attrs.lastName, 3, 25)) {
-        //        return "Last name" + this.guidNumber.toString() + " should be 3 to 25 letters long";
-        //    }
-        //
-        //    if (!checkStringLength(attrs.surName, 3, 25)) {
-        //        return "Surname " + this.guidNumber.toString() + " should be 3 to 25 letters long";
-        //    }
-        //}
+            if (!checkStringLength(attrs.lastName, 3, 25)) {
+                return "Last name" + this.guidNumber.toString() + " should be 3 to 25 letters long";
+            }
+
+            if (!checkStringLength(attrs.surName, 3, 25)) {
+                return "Surname " + this.guidNumber.toString() + " should be 3 to 25 letters long";
+            }
+        }
     });
 
     var UserListCollection = Backbone.Collection.extend({
 
-        model: UserListModel
+        model: UserListModel,
 
-        //findUserName: function (username) {
-        //
-        //    var findResult = this.find(function (user) {
-        //        return user.get("firstName") === username;
-        //    });
-        //    return findResult !== null;
-        //
-        //}
+        findUserName: function (username) {
+
+            var findResult = this.find(function (user) {
+                return user.get("firstName") === username;
+            });
+            return findResult !== null;
+
+        }
 
     });
 
@@ -107,6 +102,7 @@
                     lastName: "LastName" + i.toString(),
                     surName: "SurName" + i.toString(),
                     birthDate: new Date(1901, 0, i),
+                    birthDateLocal: "",
                     eMail: i.toString() + "EMail@stepanovv.ru",
                     phoneNumber: "+7111223334" + i.toString()
                 }
@@ -118,56 +114,53 @@
         }
     }
 
-    var userListCollection1 = new UserListCollection();
-
-    fillCollection(userListCollection1);
-
-
     //================================================VIEW==================================================
 
     var UserListView = Backbone.View.extend({
-        //el: '.page',
-        //model: new UserListModel(),
+        el: "#id-tbody-userList",
 
-        initialize: function () {
-            this.render();
+        toLocalDate: function(varDate, varLocale){
+            var options = {
+                //era: 'long',
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric'
+                //weekday: 'long',
+                //timezone: 'UTC',
+                //hour: 'numeric',
+                //minute: 'numeric',
+                //second: 'numeric'
+            };
+
+            return varDate.toLocaleString(varLocale, options).toString();
         },
 
         render: function () {
-            //var self = this;
+            var self = this;
+            this.$el.html("");
 
-            //var userListCollection1 = new UserListCollection();
+            this.model.each(
+                function(ulModel){
+                    var template = _.template($("#id-user-list-template").html());
+                    ulModel.set( 'birthDateLocal', self.toLocalDate(ulModel.get('birthDate'), "ru"));
+                    var html = template(ulModel.toJSON());
+                    self.$el.append(html);
 
-            //userListCollection1.fetch({
-            //    success: function (users) {
-            //        var template1 = _.template($('#id-user-list-template').html(), {users: users.models});
-            //        that.$el.html(template1);
-            //    }
-            //});
-
-            //this.$el.html(this.template(this.model.toJSON()));
-            //this.model.each(function (eachModel) {
-            //    var template1 = _.template($('#id-user-list-template').html(), {eachModel: eachModel.models});
-            //    var html = template1(this.model.toJSON());
-            //    self.$el.html(html);
-            //
-            //});
-
-            this.template = _.template($("#id-user-list-template").html(), { userListCollection1 : userListCollection1.models});
-            var html = this.template(this.model.toJSON());
-            this.$el.html(html);
+                }
+            );
             return this;
-        },
+        }
 
-        nextOrder: function () {
-            if (!this.length) {
-                return 1;
-            }
-            return this.last().get('order') + 1;
-        },
-
-        comparator: 'order'
+        //nextOrder: function () {
+        //    if (!this.length) {
+        //        return 1;
+        //    }
+        //    return this.last().get('order') + 1;
+        //},
+        //
+        //comparator: 'order'
     });
+
 
 
     //==============================================ROUTER====================================================
@@ -190,12 +183,18 @@
         }
     });
 
+
+    //==============================================CALCULATIONS====================================================
+
+    var userListCollection1 = new UserListCollection();
+
+    fillCollection(userListCollection1);
+
     var userListView1 = new UserListView({model: userListCollection1});
 
     var router1 = new Router();
 
     router1.on('route:routeUserList', function () {
-
         userListView1.render();
     });
 
@@ -203,51 +202,4 @@
     Backbone.history.start();
 
 
-//});
-
-
-})(jQuery);
-
-
-//(function ($) {
-//
-//    Friend = Backbone.Model.extend({
-//        //Create a model to hold friend atribute
-//        name: null
-//    });
-//
-//    Friends = Backbone.Collection.extend({
-//        //This is our Friends collection and holds our Friend models
-//        initialize: function (models, options) {
-//            this.bind("add", options.view.addFriendLi);
-//            //Listen for new additions to the collection and call a view function if so
-//        }
-//    });
-//
-//    AppView = Backbone.View.extend({
-//        el: $("body"),
-//        initialize: function () {
-//            this.friends = new Friends( null, { view: this });
-//            //Create a friends collection when the view is initialized.
-//            //Pass it a reference to this view to create a connection between the two
-//        },
-//        events: {
-//            "click #add-friend":  "showPrompt",
-//        },
-//        showPrompt: function () {
-//            var friend_name = prompt("Who is your friend?");
-//            var friend_model = new Friend({ name: friend_name });
-//            //Add a new friend model to our friend collection
-//            this.friends.add( friend_model );
-//        },
-//        addFriendLi: function (model) {
-//            //The parameter passed is a reference to the model that was added
-//            $("#friends-list").append("<li>" + model.get('name') + "</li>");
-//            //Use .get to receive attributes of the model
-//        }
-//    });
-//
-//    var appview = new AppView;
-//})(jQuery);
-//
-
+});
