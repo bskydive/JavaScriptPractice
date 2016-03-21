e7301d56657537f09956ac36d3bc8fd8c0d5a94d
bskydive <stepanovv.ru@yandex.ru>
2016-03-21 16:48:29 +0300

    hotfix-5.2.2 portfolio full design refactoring. tizer added

--- portfolio/public/js/portfolio.js
+++ portfolio/public/js/portfolio.js
@@ -6,10 +6,20 @@ $(document).ready(function () {
 
 
     function offsetResize() {
+
         var navTopHeight = $('.c-columnNavTop').height();//todo fix small bug:in small screen sizes navHeight changes after scroll at 3px
-        $("#id_containerMain").css('padding-top', navTopHeight);
-        $(".c-anchorOffset").css('top', -(navTopHeight - 30));
-        //console.log(-navTopHeight);
+        var windowWidth = $(window).width();
+
+        if ( windowWidth >= 500) {
+
+            $("#id_containerMain").css('padding-top', navTopHeight+30);
+            $(".c-anchorOffset").css('top', -(navTopHeight - 30));
+        } else {
+            $("#id_containerMain").css('padding-top', 80);
+            $(".c-anchorOffset").css('top', -20);
+        }
+
+        console.log(navTopHeight);
     }
 
     offsetResize();
