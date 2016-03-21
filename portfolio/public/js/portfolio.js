29f918cda0018a0c93441c2dda0f4e47a592c800
bskydive <stepanovv.ru@yandex.ru>
2016-03-21 18:47:12 +0300

    hotfix-5.2.2 portfolio full design refactoring. experience improved

--- portfolio/public/js/portfolio.js
+++ portfolio/public/js/portfolio.js
@@ -9,17 +9,21 @@ $(document).ready(function () {
 
         var navTopHeight = $('.c-columnNavTop').height();//todo fix small bug:in small screen sizes navHeight changes after scroll at 3px
         var windowWidth = $(window).width();
+        //var curXPos = $(document).body.scrollTop();//todo save scroll pos after resize
 
         if ( windowWidth >= 500) {
 
             $("#id_containerMain").css('padding-top', navTopHeight+50);
             $(".c-anchorOffset").css('top', -(navTopHeight - 30));
+            //window.scrollTo(curXPos+50,0);
         } else {
             $("#id_containerMain").css('padding-top', 80);
             $(".c-anchorOffset").css('top', -20);
         }
 
         //console.log(navTopHeight);
+        console.log(curXPos);
+
     }
 
     offsetResize();
